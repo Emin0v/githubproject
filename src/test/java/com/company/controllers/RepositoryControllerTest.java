45575f87284ebe5bde9499c5a0a2e27380525dc9
Emin0v <eminov-2001@inbox.ru>
2022-02-04 23:32:40 +0400

    .

--- src/test/java/com/company/controllers/RepositoryControllerTest.java
+++ src/test/java/com/company/controllers/RepositoryControllerTest.java
@@ -2,20 +2,25 @@ package com.company.controllers;
 
 import com.company.models.Repository;
 import com.company.services.RepositoryService;
+import com.company.services.dtos.requests.CreateRepositoryRequest;
 import com.company.services.dtos.resources.RepositoryResource;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,6 +32,9 @@ public class RepositoryControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private RepositoryService repositoryService;
@@ -42,5 +50,23 @@ public class RepositoryControllerTest {
                 .andExpect(jsonPath("$[0].repository").value("githubproject"));
 
     }
+
+    @Test
+    public void it_should_create_repository() throws Exception {
+        String organization = "Emin0v";
+        String repository = "githubproject";
+
+        CreateRepositoryRequest request = CreateRepositoryRequest.builder()
+                .organization(organization).repository(repository)
+                .build();
+
+        doNothing().when(this.repositoryService).create(organization,repository);
+
+        this.mockMvc.perform(post("/repositories").content(this.objectMapper.writeValueAsBytes(request)).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+                .andDo(print()).andExpect(status().isCreated());
+    }
+    
+
+
 
 }
